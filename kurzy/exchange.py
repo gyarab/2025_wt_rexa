68fb49f4a9e27df2d8f2fce46e9d37f592a1ea55
Adam Rexa <adam.rexa.s@gyarab.cz>
2026-01-22 22:32:39 +0100

    Update exchange.py

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,6 +1,6 @@
 import httpx
 
-r = https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt
+r = httpx.get('https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt')
 print(r.text)
 print(r.status)
 lines = r.text.split('\n')
